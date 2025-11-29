79344f6c9d82340bc8e60c60c02828bbe6fa80ad
brunoob35 <bruno_schmaiske_quoos@hotmail.com>
2025-11-29 11:57:09 -0300

    create users + sql

--- controller/users.go
+++ controller/users.go
@@ -1,17 +1,41 @@
 package controller
 
 import (
-	"log"
+	"Api-Aula1/models"
+	"Api-Aula1/responses"
+	"encoding/json"
+	"io"
 	"net/http"
 )
 
-func CreateUser(writer http.ResponseWriter, request *http.Request) {
+func CreateUser(w http.ResponseWriter, r *http.Request) {
 	// Ler body request
 	// Fazer o Unmarshal do json na struct do User
 	// Fazer os tratamentos necessários usando os métodos de User
 	// Enviar para o Repositório (por hora pode pular este passo, vamos trabalhar a persistencia na próxima aula)
 	// Preparar a resposta (por hora pode retornar apenas os dados do seu user ou um ID aleatório)
-	log.Print("CreateUser")
+
+	//Lê o request.Body
+	bodyRequest, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Err(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	// Descompacta (Unmarshal) o conteúdo JSON em uma Struct
+	var newUser models.Users
+	if err = json.Unmarshal(bodyRequest, &newUser); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// Chama os métodos de preparação do User
+	if err = newUser.Prepare("create"); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, newUser)
 }
 
 func FetchUser(writer http.ResponseWriter, request *http.Request) {
@@ -22,6 +46,4 @@ func UpdateUser(writer http.ResponseWriter, request *http.Request) {
 
 }
 
-func DeleteUser(writer http.ResponseWriter, request *http.Request) {
-
-}
+func DeleteUser(writer http.ResponseWriter, request *http.Request) {}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Api-Aula1
 go 1.23
 
 require (
+	github.com/badoux/checkmail v1.2.4 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 )
